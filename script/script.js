b51a2d2231061e0cd749dabc53ef44f239d59568
“Ygor <igorgarofalo@gmail.com>
2023-10-16 22:25:50 +0200

    audio player now working

--- script/script.js
+++ script/script.js
@@ -1,6 +1,27 @@
+const audiotrack = function (event) {
+  const audioSrc = document.getElementById("audio");
+  audioSrc.src = event.data[0].preview;
+
+  const artistInfo = document.querySelectorAll(".now-playing-artist");
+  artistInfo.forEach((artist) => {
+    artist.textContent = `${event.data[0].artist.name}`;
+
+    console.log((artist.textContent = `${event.data[0].artist.name}`));
+  });
+
+  const titleInfo = document.querySelectorAll(".now-playing-title");
+  titleInfo.forEach((title) => {
+    title.textContent = `${event.data[0].title}`;
+  });
+
+  const imgInfo = document.querySelectorAll(".now-playing-img");
+  imgInfo.forEach((img) => {
+    img.src = `${event.data[0].album.cover_big}`;
+  });
+};
 const searchForm = document.getElementById("search-form");
 
-searchForm.addEventListener("click", function (e) {
+searchForm.addEventListener("submit", function (e) {
   e.preventDefault();
 
   const input = document.getElementById("search-input");
@@ -20,5 +41,27 @@ searchForm.addEventListener("click", function (e) {
     })
     .then((detail) => {
       console.log(detail);
+      audiotrack(detail);
     });
+});
+
+const audio = document.getElementById("audio");
+const playPauseButton = document.querySelectorAll(".play-pause");
+const stopButton = document.getElementById("stop");
+const volumeControl = document.getElementById("volume");
+const nextButton = document.querySelector(".skip-button:nth-child(3)");
+const prevButton = document.querySelector(".skip-button:nth-child(1)");
+
+playPauseButton.forEach((button) => {
+  button.addEventListener("click", function () {
+    if (audio.paused) {
+      audio.play();
+      button.innerHTML = `<i class="bi bi-pause-circle color1"></i>
+
+      `;
+    } else {
+      audio.pause();
+      button.innerHTML = `<i class="bi bi-play-fill color1"></i>`;
+    }
+  });
 });
