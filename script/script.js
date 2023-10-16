ca9711585dc0e957605eae852752181746be6fa4
“Ygor <igorgarofalo@gmail.com>
2023-10-16 15:19:08 +0200

    added sidebar 2

--- script/script.js
+++ script/script.js
@@ -1,12 +1,24 @@
-fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=hits")
-  .then((res) => {
-    if (res.ok) {
-      return res.json();
-      console.log(res);
-    } else {
-      throw new Error("error");
-    }
-  })
-  .then((detail) => {
-    console.log(detail);
-  });
+const searchForm = document.getElementById("search-form");
+
+searchForm.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const input = document.getElementById("search-input");
+
+  let indexParam = input.value;
+  console.log(input.value);
+
+  fetch(
+    "https://striveschool-api.herokuapp.com/api/deezer/search?q=" + indexParam
+  )
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error("error");
+      }
+    })
+    .then((detail) => {
+      console.log(detail);
+    });
+});
