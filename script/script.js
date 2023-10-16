febc1f3d1f59e6e14bb754fdfb3b8d41c5ba6a39
NoemiP94 <m_emi94@hotmail.it>
2023-10-16 17:23:50 +0200

    add cards

--- script/script.js
+++ script/script.js
@@ -1,27 +1,39 @@
-// SEZIONE 1
-const addressBarContent = new URLSearchParams(location.search)
-const albumId = addressBarContent.get('albumId')
+// SEZIONE 2
 
-const renderMusic = function (arrayOfMusic) {
-  const row = document.getElementById('row-1')
-  console.log(row)
-  for (let i = 0; i <= arrayOfMusic.length; i++) {
-    const newCol = document.createElement('div')
+const renderSection2 = function (musicData) {
+  const section = document.getElementById('section2')
+  console.log(section)
 
-    newCol.classList.add('col')
-    newCol.innerHTML = `<div class="card">
-        <img src="${arrayOfMusic.data[i].artist.picture}" class="card-img-top" alt="${arrayOfMusic.data[i].artist.name}">
+  for (let i = 0; i < 6; i++) {
+    const newCol2 = document.createElement('div')
+    newCol2.classList.add('col', 'col-4', 'mb-3')
+    newCol2.innerHTML = `<div class="card d-flex flex-row align-items-center bg-body-secondary border-0">
+    <img src="${musicData.data[i].artist.picture_small}" class="card-img-top w-25" alt="${musicData.data[i].artist.name}">
+    <div class="card-body">
+      <h6 class="card-title">${musicData.data[i].artist.name}</h6>
+    </div>
+  </div>`
+    section.appendChild(newCol2)
+  }
+}
+
+// SEZIONE 3
+
+const renderSection3 = function (musicData) {
+  const row3 = document.getElementById('row-3')
+  console.log(row3)
+
+  for (let i = 0; i < 5; i++) {
+    const newCol3 = document.createElement('div')
+    newCol3.classList.add('col')
+    newCol3.innerHTML = `<div class="card border-0 bg-body-secondary my-3 p-2" style='height:230px'>
+        <img src="${musicData.data[i].artist.picture_small}" class="card-img-top" alt="${musicData.data[i].artist.name}" />
         <div class="card-body">
-          <h5 class="card-title">${arrayOfMusic.data[i].album.title}</h5>
-          <p class="card-text">${arrayOfMusic.data[i].artist.name}</p>
-          <p class="card-text">Ascolta il nuovo album di ${arrayOfMusic.data[i].artist.name}</p>
-          <a href="#" class="btn btn-primary">Play</a>
-          <a href="#" class="btn btn-primary">Salva</a>
-          <i class="bi bi-three-dots"></i>
+          <p class="card-title fw-bold" style='font-size:15px'>${musicData.data[i].artist.name}</p>
+          <p class="card-text">${musicData.data[i].album.title}</p>
         </div>
       </div>`
-    row.appendChild(newCol)
-    console.log(newCol)
+    row3.appendChild(newCol3)
   }
 }
 
@@ -35,7 +47,9 @@ const getMusic = function () {
       }
     })
     .then((music) => {
-      renderMusic(music)
+      console.log(music)
+      renderSection2(music)
+      renderSection3(music)
     })
     .catch((err) => {
       console.log('Si è verificato un errore', err)
@@ -43,3 +57,52 @@ const getMusic = function () {
 }
 
 getMusic()
+
+// DETTAGLIO
+
+const addressBarContent = new URLSearchParams(location.search)
+const musicId = addressBarContent.get('musicId')
+
+const generateDetails = function (details) {
+  const row1 = document.getElementById('row-1')
+  row1.innerHTML = `<div class="col col-4">
+     <img
+     src="assets/imgs/main/image-13.jpg"
+      alt=""
+      class="img-fluid"/>
+    </div>
+   <div class="col-8">
+    <h6>ALBUM</h6>
+     <h1>Titolo</h1>
+     <p>Artista</p>
+    <p>descrizione</p>
+     <div class="d-flex align-items-center">
+       <a href="#" class="btn btn-success rounded-4 me-2 px-4"
+         >Play</a
+      >
+      <a
+        href="#"
+         class="btn btn-black btn-outline-light rounded-4 mx-2 px-4"
+         >Salva</a
+       >
+       <i class="bi bi-three-dots ms-3"></i>
+     </div>
+  </div>`
+}
+
+const getDetails = function () {
+  fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=' + musicId)
+    .then((res) => {
+      if (res.ok) {
+        return res.json()
+      } else {
+        throw new Error('Errore nel caricamento dei dettagli')
+      }
+    })
+    .then((musicData) => {
+      generateDetails(musicData)
+    })
+    .catch((err) => {
+      console.log('Errore', err)
+    })
+}
