0fba7a9648269378731c461aee238509fe501603
IsuruFerna <isurumadushan.ferna@gmail.com>
2023-10-17 13:30:18 +0200

    color solved

--- script/album.js
+++ script/album.js
@@ -1,5 +1,32 @@
 console.log("sono collegato");
 
+const renderAlbum = function (album) {
+   const albumName = document.getElementById("album-name");
+   const recordType = document.getElementById("record-type");
+   const albumArtist = document.getElementById("album-artist");
+
+   // setting data
+   albumName.innerText = album.title;
+   recordType.innerText = album.record_type.toUpperCase();
+   albumArtist.setAttribute("src", `${album.artist.picture}`);
+
+   // setting height of bg black
+   const bgBlack = document.querySelector(".album-bg-black");
+   const fullHeight = document.querySelector(".tracks-and-header");
+   const musicBar = document.getElementById("music-bar");
+   const header = document.getElementsByTagName("header");
+
+   // fullHeight.style.height =
+   //    window.innerHeight - musicBar.innerHeight - header.innerHeight + "px";
+
+   bgBlack.style.height =
+      fullHeight.clientHeight -
+      window.innerHeight / 2 -
+      musicBar.innerHeight -
+      header.innerHeight +
+      "px";
+};
+
 const loadAlbum = function (id = 75621062) {
    fetch("https://striveschool-api.herokuapp.com/api/deezer/album/" + id)
       .then((response) => {
@@ -10,6 +37,7 @@ const loadAlbum = function (id = 75621062) {
       })
       .then((respone) => {
          console.log("Fetch went fine!", respone);
+         renderAlbum(respone);
       })
       .catch((err) => console.log("Fetching issue", err));
 };
