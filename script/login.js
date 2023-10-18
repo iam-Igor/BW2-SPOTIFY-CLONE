3206b59a2c90b1644cee3a57f9519f681371c3f1
“Ygor <igorgarofalo@gmail.com>
2023-10-18 13:26:16 +0200

    fixed login page with checkbox

--- script/login.js
+++ script/login.js
@@ -1,15 +1,35 @@
-let email = 'team8@epicode.com'
-let password = 'epicode2023'
+let email = "team8@epicode.com";
+let password = "epicode2023";
 
-const form = document.getElementById('login-form')
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const emailInput = document.getElementById('login-email')
-  const passwordInput = document.getElementById('login-password')
+const emailInput = document.getElementById("login-email");
+const passwordInput = document.getElementById("login-password");
+
+const form = document.getElementById("login-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
   if (emailInput.value === email && passwordInput.value === password) {
-    location.assign('main.html')
+    location.assign("main.html");
   } else {
-    const loginalert = document.getElementsByClassName('loginalert')[0]
-    loginalert.classList.remove('d-none')
+    const loginalert = document.getElementsByClassName("loginalert")[0];
+    loginalert.classList.remove("d-none");
   }
-})
+});
+
+const showPassword = function () {
+  const checkbox = document.getElementById("checkbox");
+  const eyeicon = document.getElementById("eye-icon");
+  checkbox.addEventListener("change", function () {
+    if (this.checked) {
+      passwordInput.type = "text";
+      eyeicon.classList.remove("bi-eye-slash");
+      eyeicon.classList.add("bi-eye");
+    } else {
+      passwordInput.type = "password";
+      eyeicon.classList.add("bi-eye-slash");
+      eyeicon.classList.remove("bi-eye");
+    }
+  });
+};
+
+showPassword();
