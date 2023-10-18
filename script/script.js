0804bdca567f87f9ccc97ccac27086bc45e8ad66
NoemiP94 <m_emi94@hotmail.it>
2023-10-18 09:43:36 +0200

    fix

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
 // DETTAGLIO
 
 const generateDetails = function (details) {
-  console.log(details);
-  const row1 = document.getElementById("row-1");
+  console.log(details)
+  const row1 = document.getElementById('row-1')
   for (let j = 0; j < details.data.length; j++) {
     row1.innerHTML = `<div class="col col-4">
      <img
@@ -13,7 +13,7 @@ const generateDetails = function (details) {
    <div class="col-8 pt-3">
     <h6>${details.data[j].title}</h6>
      <h1>${details.data[j].album.title}</h1>
-     <a href='./details.html?musicId=${details.data[j].artist.id}'>${details.data[j].artist.name}</a>
+     <a href='./album.html?musicId=${details.data[j].artist.id}'>${details.data[j].artist.name}</a>
     
      <div class="d-flex align-items-center">
        <a href="#" class="btn btn-success rounded-4 me-2 px-4 play-pause"
@@ -26,15 +26,15 @@ const generateDetails = function (details) {
        >
        <i class="bi bi-three-dots ms-3"></i>
      </div>
-  </div>`;
+  </div>`
   }
-};
+}
 
 // SEZIONE 2
 
 const renderSection2 = function (musicData) {
-  const section = document.getElementById("section2");
-  console.log(musicData.data);
+  const section = document.getElementById('section2')
+  console.log(musicData.data)
   for (let i = 0; i < 6; i++) {
     section.innerHTML = `<div class="col mb-3 col-md-4">
     <div
@@ -145,14 +145,14 @@ const renderSection2 = function (musicData) {
       </a>
       </div>
     </div>
-  </div>`;
+  </div>`
   }
-};
+}
 
 // SEZIONE 3
 
 const renderSection3 = function (musicData) {
-  const row3 = document.getElementById("row-3");
+  const row3 = document.getElementById('row-3')
 
   for (let i = 0; i < 5; i++) {
     row3.innerHTML = `<div class="col">
@@ -265,94 +265,94 @@ const renderSection3 = function (musicData) {
         </p>
       </div>
     </div>
-  </div>`;
+  </div>`
   }
-};
+}
 
 // SEZIONE PREFERITI
 const generateFavourites = function (musicData) {
-  const ul = document.getElementById("favourites");
+  const ul = document.getElementById('favourites')
   for (let k = 0; k < musicData.data.length; k++) {
-    const newLi = document.createElement("li");
-    newLi.classList.add("lh-base", "text-secondary", "link-light");
-    newLi.innerText = `${musicData.data[k].title}`;
-    ul.appendChild(newLi);
+    const newLi = document.createElement('li')
+    newLi.classList.add('lh-base', 'text-secondary', 'link-light')
+    newLi.innerText = `${musicData.data[k].title}`
+    ul.appendChild(newLi)
   }
-};
+}
 const audiotrack = function (event) {
-  console.log(event);
-  const audioSrc = document.getElementById("audio");
-  audioSrc.src = event.data[0].preview;
+  console.log(event)
+  const audioSrc = document.getElementById('audio')
+  audioSrc.src = event.data[0].preview
 
-  console.log((audioSrc.src = event.data[0].preview));
-  const artistInfo = document.querySelectorAll(".now-playing-artist");
+  console.log((audioSrc.src = event.data[0].preview))
+  const artistInfo = document.querySelectorAll('.now-playing-artist')
   artistInfo.forEach((artist) => {
-    artist.textContent = event.data[0].artist.name;
-  });
+    artist.textContent = event.data[0].artist.name
+  })
 
-  const titleInfo = document.querySelectorAll(".now-playing-title");
+  const titleInfo = document.querySelectorAll('.now-playing-title')
   titleInfo.forEach((title) => {
-    title.textContent = event.data[0].title;
-  });
+    title.textContent = event.data[0].title
+  })
 
-  const imgInfo = document.querySelectorAll(".now-playing-img");
+  const imgInfo = document.querySelectorAll('.now-playing-img')
   imgInfo.forEach((img) => {
-    img.src = event.data[0].album.cover_big;
-  });
-};
+    img.src = event.data[0].album.cover_big
+  })
+}
 
-const form = document.getElementById("form-search");
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const input = document.getElementById("search-input");
-  let index = input.value;
-  fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=" + index)
+const form = document.getElementById('form-search')
+form.addEventListener('submit', function (e) {
+  e.preventDefault()
+  const input = document.getElementById('search-input')
+  let index = input.value
+  fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=' + index)
     .then((res) => {
       if (res.ok) {
-        return res.json();
+        return res.json()
       } else {
-        throw new Error("Errore nel contattare il server");
+        throw new Error('Errore nel contattare il server')
       }
     })
     .then((music) => {
-      renderSection2(music);
-      renderSection3(music);
-      generateFavourites(music);
+      renderSection2(music)
+      renderSection3(music)
+      generateFavourites(music)
 
-      audiotrack(music);
-      generateDetails(music);
+      audiotrack(music)
+      generateDetails(music)
     })
     .catch((err) => {
-      console.log("Si è verificato un errore", err);
-    });
-});
+      console.log('Si è verificato un errore', err)
+    })
+})
 
-const audio = document.getElementById("audio");
+const audio = document.getElementById('audio')
 
-const playPauseButton = document.querySelectorAll(".play-pause");
-const stopButton = document.getElementById("stop");
-const volumeControl = document.getElementById("volume");
-const nextButton = document.querySelector(".skip-button:nth-child(3)");
-const prevButton = document.querySelector(".skip-button:nth-child(1)");
+const playPauseButton = document.querySelectorAll('.play-pause')
+const stopButton = document.getElementById('stop')
+const volumeControl = document.getElementById('volume')
+const nextButton = document.querySelector('.skip-button:nth-child(3)')
+const prevButton = document.querySelector('.skip-button:nth-child(1)')
 
 playPauseButton.forEach((button) => {
-  button.addEventListener("click", function () {
+  button.addEventListener('click', function () {
     if (audio.paused) {
-      audio.play();
-      button.innerHTML = `<i class="bi bi-pause-circle color1 fs-1 fs-md-4"></i>`;
+      audio.play()
+      button.innerHTML = `<i class="bi bi-pause-circle color1 fs-1 fs-md-4"></i>`
     } else {
-      audio.pause();
-      button.innerHTML = `<i class="bi bi-play-fill color1 fs-1 fs-md-4"></i>`;
+      audio.pause()
+      button.innerHTML = `<i class="bi bi-play-fill color1 fs-1 fs-md-4"></i>`
     }
-  });
-});
+  })
+})
 
 // SEZIONE CERCA MOBILE
 
-const searchButton2 = document.getElementById("search2");
+const searchButton2 = document.getElementById('search2')
 
-searchButton2.addEventListener("click", function () {
-  const navBar = document.getElementById("navbar");
-  navBar.classList.remove("d-none");
-  navBar.classList.add("slide-in-blurred-top");
-});
+searchButton2.addEventListener('click', function () {
+  const navBar = document.getElementById('navbar')
+  navBar.classList.remove('d-none')
+  navBar.classList.add('slide-in-blurred-top')
+})
